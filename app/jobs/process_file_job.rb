0b0975c25dd13aa349a2602a7ecde07b10c6997a
Jorge Coutinho <jorgecsn@id.uff.br>
2024-12-31 15:38:15 -0300

    fix process file job to prod

--- app/jobs/process_file_job.rb
+++ app/jobs/process_file_job.rb
@@ -2,10 +2,11 @@ class ProcessFileJob < ApplicationJob
   queue_as :default
 
   def perform(file_upload)
-    file_path = ActiveStorage::Blob.service.send(:path_for, file_upload.file.key)
-
-    File.foreach(file_path) do |line|
-      Tag.create!(name: line.strip)
+    file = file_upload.file
+    file.download do |content|
+      content.each_line do |line|
+        Tag.create!(name: line.strip)
+      end
     end
   end
 end
