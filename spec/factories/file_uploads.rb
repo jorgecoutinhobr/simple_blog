f5e6f81d40b0ae0aedeff2dc0e9265eb3392493e
Jorge Coutinho <jorgecsn@id.uff.br>
2025-01-03 18:46:55 -0300

    add process_job spec

--- spec/factories/file_uploads.rb
+++ spec/factories/file_uploads.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :file_upload do
+    file { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/tag.txt'), 'text/plain') }
   end
 end
--- spec/jobs/process_file_job_spec.rb
+++ spec/jobs/process_file_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ProcessFileJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:file_upload) { create(:file_upload) }
+  let(:file_content) { "Tag1\nTag2\nTag3" }
+
+  before do
+    allow(file_upload.file).to receive(:download).and_yield(file_content)
+  end
+
+  it 'creates tags from file content' do
+    expect { described_class.perform_now(file_upload) }.to change(Tag, :count).by(3)
+    expect(Tag.pluck(:name)).to include('Tag1', 'Tag2', 'Tag3')
+  end
 end
